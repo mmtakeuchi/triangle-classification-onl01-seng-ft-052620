ecd58cf58b47fe2ab1b46787019c913b6d62cb9d
mmtakeuchi <mikemtaki@gmail.com>
2020-06-09 20:06:05 -0700

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
-  # write code here
+  attr_accessor :side_1, :side_2, :side_3, :equilateral, :isosceles, :scalene
+
+  @@all = []
+
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def kind
+    if (side_1 <= 0 || side_2 <= 0 || side_3 <= 0) || (side_1 + side_2 <= side_3) || (side_1 + side_3 <= side_2) || (side_2 + side_3 <= side_1)
+      raise TriangleError
+    elsif side_1 == side_2 && side_1 ==side_3 && side_2 == side_3
+      :equilateral
+    elsif side_1 == side_2 && ((side_1 || side_2) != side_3)
+      :isosceles
+    elsif side_1 == side_3 && ((side_1 || side_3) != side_2)
+      :isosceles
+    elsif side_2 == side_3 && ((side_2 || side_3) != side_1)
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "sides cannot be zero or negative values, and two sides must exceed the length of the third side."
+    end
+  end
 end
